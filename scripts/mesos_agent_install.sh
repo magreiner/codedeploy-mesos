3beefff5fc0251c7559e32c043b8161736aa612e
Matthias Greiner <Matthias.Greiner@stud.uni-heidelberg.de>
2016-04-06 11:04:35 +0200

    mesos agent test

---
+++ scripts/mesos_agent_install.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+param_mesos_master_ip="192.168.10.126"
+
+#
+# DOCKER
+#
+apt-get update
+apt-get --yes install apt-transport-https ca-certificates
+
+apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
+apt-get update
+apt-get --yes install linux-image-extra-$(uname -r)
+apt-get --yes install apparmor
+apt-get --yes install docker-engine
+
+usermod -aG docker ubuntu
+
+service docker start
+
+exit 1
+#
+# MESOS CLIENT
+#
+apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
+DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
+CODENAME=$(lsb_release -cs)
+echo "deb http://repos.mesosphere.com/${DISTRO} ${CODENAME} main" > /etc/apt/sources.list.d/mesosphere.list
+apt-get --yes update
+apt-get --yes install mesos
+echo "$param_mesos_master_ip  mesosmaster" >> /etc/hosts
+mkdir -p /var/lib/mesos
+chown ubuntu:ubuntu /var/lib/mesos
+
+echo 'docker,mesos' > /etc/mesos-slave/containerizers
+echo '5mins' > /etc/mesos-slave/executor_registration_timeout
+
+service mesos-slave start
+
+#MY_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+#screen -dmS mesos-agent bash -c  "/usr/sbin/mesos-slave --master=mesosmaster:5050 --ip=$MY_IP --work_dir=/var/lib/mesos"

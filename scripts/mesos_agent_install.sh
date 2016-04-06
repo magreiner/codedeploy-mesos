821ab77722a106f05eb153558f56e8acff0da4d5
Matthias Greiner <Matthias.Greiner@stud.uni-heidelberg.de>
2016-04-06 18:48:17 +0200

    connect to master via screen

--- scripts/mesos_agent_install.sh
+++ scripts/mesos_agent_install.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 MIN_MASTER_INSTANCES=1
+MASTER_INSTANCE_TAGNAME="AS_Master"
+
+AZ="$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)"
+REGION=$(${AZ::-1})
 
 # Looking for other master instances for HA (Zookeeper)
-MASTER_IPS=$(aws ec2 describe-instances --region eu-west-1 --filters "Name=tag:Name,Values=AS_Master" | jq '. | {ips: .Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddress}' | grep "\." | cut -f4 -d'"')
-MASTER_INSTANCES_ONLINE=$(echo "$MASTER_IPS" | wc -l)
+MASTER_IPS=$(aws ec2 describe-instances --region $REGION --filters "Name=tag:Name,Values=$MASTER_INSTANCE_TAGNAME" | jq '. | {ips: .Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddress}' | grep "\." | cut -f4 -d'"')
+MASTER_INSTANCES_ONLINE=$(echo "$MASTER_IPS" | grep "\." | wc -l)
+
 while [ "$MASTER_INSTANCES_ONLINE" -lt "$MIN_MASTER_INSTANCES" ]; do
   sleep 2
   echo "Waiting for more master instances. ($MASTER_INSTANCES_ONLINE/$MIN_MASTER_INSTANCES online)"
-  MASTER_IPS=$(aws ec2 describe-instances --region eu-west-1 --filters "Name=tag:Name,Values=AS_Master" | jq '. | {ips: .Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddress}' | grep "\." | cut -f4 -d'"' | head -n1)
-  MASTER_INSTANCES_ONLINE=$(echo "$MASTER_IPS" | wc -l)
+  MASTER_IPS=$(aws ec2 describe-instances --region $REGION --filters "Name=tag:Name,Values=$MASTER_INSTANCE_TAGNAME" | jq '. | {ips: .Reservations[].Instances[].NetworkInterfaces[].PrivateIpAddress}' | grep "\." | cut -f4 -d'"' | head -n1)
+  MASTER_INSTANCES_ONLINE=$(echo "$MASTER_IPS" | grep "\." | wc -l)
 done
 FIRST_MASTER_IP=$(echo "$MASTER_IPS" | head -n1)
 
@@ -46,7 +51,7 @@ chown ubuntu:ubuntu /var/lib/mesos
 echo 'docker,mesos' > /etc/mesos-slave/containerizers
 echo '5mins' > /etc/mesos-slave/executor_registration_timeout
 
-service mesos-slave start
+# service mesos-slave start
 
-# MY_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
-# screen -dmS mesos-agent bash -c  "/usr/sbin/mesos-slave --master=$MY_IP:5050 --ip=$MY_IP --work_dir=/var/lib/mesos"
+MY_IP=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
+screen -dmS mesos-agent bash -c  "/usr/sbin/mesos-slave --master=mesosmaster:5050 --ip=$MY_IP --work_dir=/var/lib/mesos"

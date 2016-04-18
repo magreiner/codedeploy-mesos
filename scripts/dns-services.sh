a38e39bbdca0100b8e9511416049ad171ec07622
Matthias Greiner <Matthias.Greiner@stud.uni-heidelberg.de>
2016-04-18 20:38:39 +0200

    removed consul

--- scripts/dns-services.sh
+++ scripts/dns-services.sh
@@ -34,7 +34,3 @@ cat > /etc/mesos-dns/config.json << EOF
   "IPSources": ["netinfo", "mesos", "host"]
 }
 EOF
-
-# apt-get install -yq dnsmasq
-# echo "server=/consul/127.0.0.1#8600" | tee /etc/dnsmasq.d/10-consul
-# service dnsmasq restart
